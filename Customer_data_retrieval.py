8ab81b10ae7f3154f850e0eade84faa990b725c1
Pallavi Ninad Kakade <30298574+pallukakade@users.noreply.github.com>
2021-03-31 20:35:39 +0100

    Add files via upload

---
+++ Customer_data_retrieval.py
@@ -0,0 +1,105 @@
+'''
+Description:
+   Calculate the distance to Dublin office from the
+   given co-ordinates(latitude, longitude) of the customer
+   provided in the customer.txt file and list the names and
+   user_ids customers of the customers living within 100km(sorted list
+   User ID (ascending).
+computation1 = sin(latitide_diff / 2)**2 + cos(dub_off_latitude) * \
+                           cos(latitide1) * sin(longitude_diff / 2)**2
+            computation2 = 2 * asin(sqrt(computation1))
+            distance_in_kms = round(computation2 * radius_in_kms, 2)
+
+
+Haversine formula::
+        computation1 = sin(latitide_diff / 2)**2 + cos(dub_off_latitude) * \
+                           cos(latitide1) * sin(longitude_diff / 2)**2
+        computation2 = 2 * asin(sqrt(computation1))
+        distance_in_kms = round(computation2 * radius_in_kms, 2)
+   where,
+      longitude_diff = longitude1 - dub_off_longitude (difference of latitude)
+      latitide_diff = latitide1 - dub_off_latitude  (difference of longitude)
+      radius_in_kms is radius of earth i.e 6371 KM or 3961 miles
+      and distance_in_kms is the distance computed between two points.
+
+@Since    :  March 2021.
+@Author   :  Pallavi Ninad Kakade
+
+'''
+
+import ast
+from math import cos, sqrt, sin, radians, asin
+from pprint import pprint
+
+
+class Customer_data_retrieval:
+    def customer_details(self):
+        """
+            @tms_test_steps:
+                    @step: Convert the coordinates of Dublin office to radians.
+                    @result: The coordinates of Dublin office are converted to radians.
+                    @step: Open the customer.txt file in read mode.
+                    @result: The customer.txt file is opened in read mode
+                    @step: Create an empty customer list to store the user_id and name
+                           of customers whose distance is <=100.
+                    @result: An empty customer list is created.
+                    @step: using with statement open the file and read the data
+                    @result: File is opened and read successfully
+                    @step: Obtain the cordinates (latitude and longitude) from the file
+                    @result: Obtained the latitude and longitude from the file
+                    @step: Calculate the Distance using Haversine Formulae
+                    @result:The Distance is calculated successfully
+                    @step: Check if Distance<=100 then retrieve the name and
+                           user_id of the customer
+                    @result: The name and user_id of the customer is retrieved.
+                    @step: Arrange the list of customers in ascending order of user_ids.
+                    @result: The list of customers is arranged in ascending order of user_ids.
+                @tms_pre-condition: Save the customer.txt file on local machine and
+                                    update the path of the file in code(input_file)
+        """
+        # dublin office latitude and longitude co-ordinates.
+        dub_off_latitude, dub_off_longitude = radians(53.339428), radians(-6.257664)
+
+        # empty customer list to store the user_id and name
+        # of customers whose distance is <=100
+        customer_dict = {}
+
+        # Please provide the path  of the customer data.txt file stored on your local machine
+        # input_file = open("/path/to/customers.txt", "r")
+        input_file = open("/home/pallavi.kakade@ammeon.com/python_practice/customers.txt", "r")
+        self.customer_dict = {}
+
+        # retrieve  line of the customer.txt file
+        with input_file as file1:
+            for line in file1:
+                line1 = ast.literal_eval(line)
+                # Retrieving the latitude and longitude from the file
+                latitide1 = radians(float(line1.get('latitude')))
+                longitude1 = radians(float(line1.get('longitude')))
+
+                # Radius of earth in kilometers.
+                radius_in_kms = 6371
+
+                # Find distance of customer from the
+                # Dublin office using Haversine Formulae.
+                longitude_diff = longitude1 - dub_off_longitude
+                latitide_diff = latitide1 - dub_off_latitude
+
+                computation1 = sin(latitide_diff / 2) ** 2 + cos(dub_off_latitude) * \
+                               cos(latitide1) * sin(longitude_diff / 2) ** 2
+                computation2 = 2 * asin(sqrt(computation1))
+                distance_in_kms = round(computation2 * radius_in_kms, 2)
+
+                if distance_in_kms <= 100:
+                    customer_dict[line1['user_id']] = str(line1['name'])
+
+        print('The full list of customers with their names and '
+              'user_ids(sorted in ascending order) of matching customers '
+              '(within 100km from Dublin office) are :: - ')
+        pprint(dict(sorted(customer_dict.items())))
+
+# Calling the class customer
+customer_list = Customer_data_retrieval()
+customer_list.customer_details()
+
+
---
+++ readme.txt
@@ -0,0 +1,42 @@
+###################################################################################################
+##                                                                                               ##
+##                     **  Read ME  **                                                           ##
+##                     **  Execute Python scripts in the                                         ##
+##                            1. terminal or                                                     ##
+##                            2. an IDE                                                          ##
+###################################################################################################
+
+I. Run from terminal ::
+      
+1. Check if  you have python installed on a your machine by running following command on Terminal:  
+        python --version
+
+2. If Python is not installed then you can download it and install python using commad :
+       https://www.python.org/downloads/
+3. Once the python is installed. Then Save the main.py file and Open your command line.Navigate to the directory where you saved your file.
+
+4. Save the customer.txt to local machine. And update the path of that file in the code and run the program:
+      python main.py
+5.  The python program is executed successfully.
+
+
+##########
+
+II . Run the code from an PyCharm IDE     
+   
+   
+1. To run a Python script from an pycharm IDE, Download the pycharm first(https://www.jetbrains.com/pycharm/download/#section=linux)
+2. Start a New project. Once the project is created add your .py files (or create them in the IDE).
+3. Save the customer.txt to local machine. And update the path of that file in the code and run the program by  pressing 'RUN'.
+
+In the PyCharm IDE:
+
+    a. Start project
+        Welcome screen opens, click Create New Project.
+        On the main menu, choose File | New Project.
+    b. Select Python interpreter
+        Choose Python version from the list. Use 3.x
+    c. Click create
+    d. Add new Python file (File new) and add hello.py
+    e. Click the green triangle to start the program. Another option is to click right mouse button on your Python file and selecting run.
+
